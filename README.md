adda18063bc5c2e8a4cb4ddd1e0b84c3378ce317
hiroshisiq <hiroshi.siq@gmail.com>
2016-12-07 03:54:46 -0200

    cleaning working directory

--- README.md
+++
@@ -1,2 +0,0 @@
-# VHDL_MIPS16e
-A VHDL MIPS16e multicycle implementation for discipline of hardware description language
--- microprocessor/mips16e.vhd
+++
@@ -1,9 +0,0 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-
-ENTITY mips16e IS
-	PORT(clk, reset        : IN  STD_LOGIC;
-		 instruction       : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
-		 pc                : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-
-		);
